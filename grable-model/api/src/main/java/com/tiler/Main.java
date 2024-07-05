b251c3cd810bda6e7d3deb49032902914fcd0d76
Victoria <molnisspsss@gmail.com>
2024-07-05 14:22:47 +0300

    Initial commit

---
+++ grable-model/api/src/main/java/com/tiler/Main.java
@@ -0,0 +1,14 @@
+package com.tiler;
+
+import com.tiler.MyEntity;
+import com.tiler.MyService;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println(myService.getMyEntity());
+        System.out.println(myService.setMyEntity(new MyEntity("second")));
+        System.out.println(myService.getMyEntity());
+    }
+}
